12c51d8b5d29c0286103fc1e013f26c162fb6333
Bams29 <sarmientobryan2911@gmail.com>
2024-01-30 06:11:09 -0500

    jj

--- day3/money change.py
+++ day3/money change.py
@@ -1,12 +1,12 @@
 Credit=int(input("insert credit: "))
 Money = [10,5,1]
 billsReturned = []
- 
+
 def change(Credit):
     for i in Money:
         while Credit >=i:
             billsReturned.append(i)
             Credit -= i
     print(billsReturned)
- 
+
 change(Credit)
