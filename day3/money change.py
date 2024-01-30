d05f74ca205517fd55035ef18bc1cf7607e02885
Bams29 <sarmientobryan2911@gmail.com>
2024-01-30 06:45:34 -0500

    un try

--- day3/money change.py
+++ day3/money change.py
@@ -1,12 +1,20 @@
-Credit=int(input("insert credit: "))
-Money = [10,5,1]
-billsReturned = []
 
-def change(Credit):
-    for i in Money:
-        while Credit >=i:
-            billsReturned.append(i)
+
+try:
+    Credit=int(input("insert credit: "))
+
+    
+    Money = [10,5,1]
+    billsReturned = []
+
+    def change(Credit):
+        for i in Money:
+            while Credit >=i:
+                billsReturned.append(i)
             Credit -= i
     print(billsReturned)
 
-change(Credit)
+
+    change(Credit)
+except ValueError:
+    print("need a number")
