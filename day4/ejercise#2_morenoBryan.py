e75f9db98f5e277d2f8d33a5488aff4d11178436
Bams29 <sarmientobryan2911@gmail.com>
2024-01-31 16:42:00 -0500

    finally

--- day4/ejercise#2_morenoBryan.py
+++ day4/ejercise#2_morenoBryan.py
@@ -1,15 +1,17 @@
 import itertools
+
 def nearest_leg_lengths(coin_thicknesses, table_heights):
     def possible_leg_lengths(coin_thicknesses, target_height):
-        min_leg_length = max(coin_thicknesses)
-        max_leg_length = sum(coin_thicknesses)
-        possible_lengths = []
+        min_leg_length = float('inf')
+        max_leg_length = 0
         for i in range(1, len(coin_thicknesses) + 1):
             for combination in itertools.combinations(coin_thicknesses, i):
                 total_thickness = sum(combination)
-                if min_leg_length <= total_thickness <= target_height:
-                    possible_lengths.append(total_thickness)
-        return min(possible_lengths), max(possible_lengths)
+                if min_leg_length > total_thickness:
+                    min_leg_length = total_thickness
+                if max_leg_length < total_thickness <= target_height:
+                    max_leg_length = total_thickness
+        return min_leg_length, max_leg_length
 
     results = []
     for table_height in table_heights:
@@ -27,6 +29,16 @@ def read_input():
         yield coin_thicknesses, table_heights
 
 for coin_thicknesses, table_heights in read_input():
+    results = nearest_leg_lengths(coin_thicknesses, table_heights)
+    for min_leg_length, max_leg_length in results:
+        print(min_leg_length, max_leg_length)
+
+while True:
+    n, t = map(int, input().split())
+    if n == 0 and t == 0:
+        break
+    coin_thicknesses = [int(x) for x in input().split()]
+    table_heights = [int(x) for x in input().split()]
     results = nearest_leg_lengths(coin_thicknesses, table_heights)
     for min_leg_length, max_leg_length in results:
         print(min_leg_length, max_leg_length)
