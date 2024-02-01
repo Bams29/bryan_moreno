02a8f6a375a39928959e009fe03e1fc16a83a832
Bams29 <sarmientobryan2911@gmail.com>
2024-02-01 13:34:34 -0500

    #2

---
+++ day5/ejercise#2_MorenoBryan.py
@@ -0,0 +1,25 @@
+#correction
+
+num = int(input())
+
+n_num = num * (-1)
+print(n_num)
+
+
+if num > 10000:
+    print(True)
+else:
+    print(False) 
+
+
+
+
+
+
+
+#BUGS LIST
+
+#not definet the variable num
+#did not use the variable "print" correctly
+#in "negate" and "big" was calling a non-existent "b" value
+#deployed by Bryan Moreno mann.co member
