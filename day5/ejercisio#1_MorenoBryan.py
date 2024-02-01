fd5fee1fc3adced3757f35a2154c6d0ba8b63ada
Bams29 <sarmientobryan2911@gmail.com>
2024-02-01 09:22:36 -0500

    primero

---
+++ day5/ejercisio#1_MorenoBryan.py
@@ -0,0 +1,16 @@
+products = {"papa":6000,"cola":3000,"pitaya":5000,"pera":3000,"hals":500,"yupi":2500}
+
+for i in products:
+    print(i)
+
+chose = input("Producto?:")
+chose2= int(input("Cuanto?"))
+
+valorFinal= products[chose] * chose2
+print(valorFinal)
+
+#deployed by BRYAN MORENO
+
+
+
+
